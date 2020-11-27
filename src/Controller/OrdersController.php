085c03e377fc0d848277576acdbe81c991f8b317
erdem.yenier <erdem.yenier@kroppa.com>
2020-11-27 10:45:49 +0300

    tüm siparişleri listeleme

--- src/Controller/OrdersController.php
+++ src/Controller/OrdersController.php
@@ -12,11 +12,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class OrdersController extends AbstractController
 {
     /**
-     * @Route("/orders", name="orders", methods={"POST"})
+     * @Route("/orders-new", name="orders-new", methods={"POST"})
      */
     public function insert(Request $request): Response
     {   
-        
         $order = new Orders();
         $order->setCustomerId($request->get('id'));
         $order->setOrderCode(mt_rand());
@@ -33,7 +32,65 @@ class OrdersController extends AbstractController
         } else {
             return new Response('Hata Oluştu');
         }
+    }
+    /**
+     * @Route("/orders-list", name="orders-list", methods={"POST"})
+     */
+    public function order_list(Request $request , EntityManagerInterface $em): Response
+    {   
         
+        $qb = $em->createQueryBuilder('o')->where('o.id = :id')
+        ->setParameter('id', $request->get('id'))
+        ->orderBy('o.id', 'ASC');
+
+        $query = $qb->getQuery();
+        return $query->execute();
+        
+        
+    }
+    /**
+     * @Route("/orders", name="orders", methods={"GET"})
+     */
+    public function show($order_code): Response
+    {   
+        $order = $this->getDoctrine()->getRepository(Orders::class)->findOneBy([
+            'order_code' => $order_code
+        ]);
+        if (!$order) {
+            return $this->json(['message' => 'Belirtilen sipariş numarasında sipariş bulunamadı '.$order_code]);
+        }
+        return $this->json([
+            'Sipariş' => $order->getProdutId(),
+            'Sipariş Adeti' => $order->getQuantity(),
+            'Sipariş Adresi' => $order->getAddress(),
+            'Sipariş Tarihi' => $order->getShippingDate()
+        ]);
+        
+    }
+    /**
+     * @Route("/orders", name="orders", methods={"POST"})
+     */
+    public function edit(Request $request, $order_code): Response
+    {   
+        $entityManager = $this->getDoctrine()->getManager();
+        $order = $this->getDoctrine()->getRepository(Orders::class)->findOneBy([
+            'order_code' => $order_code
+        ]);
+        if (!$order) {
+            return $this->json(['message' => 'Belirtilen sipariş numarasında sipariş bulunamadı '.$order_code]);
+        }
+        $order->setCustomerId($request->get('id'));
+        $order->setProdutId($request->get('product_id'));
+        $order->setQuantity($request->get('quantity'));
+        $order->setAddress($request->get('address'));
+        $order->setShippingDate(new \DateTime());
+        $entityManager->flush();
+        
+        if ($entityManager) {
+            return $this->json(['message' => 'Başarılı bir şekilde güncellenmiştir.'.$order->getOrderCode()]);
+        }
+        else
+            return $this->json(['message' => 'HATA']);
         
     }
 }
